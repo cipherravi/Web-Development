b6157ab0cf420c782acc89dadd2d09de834f3f4d
Ravi-9431 <ravi4personal9431@gmail.com>
2024-06-14 16:12:14 +0530

    practice

--- Practice/june_2024/Practice_1/Javascript/app.js
+++ Practice/june_2024/Practice_1/Javascript/app.js
@@ -1,1 +1,68 @@
-console.log("Hello Wolrd!");
+// console.log("Hello Wolrd!");
+// let name = "ravi kumar yadav";
+// console.log(name.split(" ").join("_"));
+// console.log(name.includes(" "));
+// let number = "50";
+// number = +number;
+// number = Number(number);
+// number = parseInt(number);
+// console.log(number);
+// console.log(typeof number);
+// let a = 1;
+// do {
+//   console.log("first", a);
+//   a++;
+// } while (a < 5);
+// for (i = 1; i < 5; i++) {
+//   console.log("Hello", i);
+// }
+
+// for (let i = 0; i < 5; i++) {
+//   console.log("*", i);
+//   for (let y = 0; y < 5; y++) {
+//     console.log("$", y);
+//   }
+// }
+
+// let arr = {};
+// console.log(typeof arr);
+
+// let person = { firstName: "Ravi" };
+// person.firstName = "Raju";
+// console.log(person);
+
+// function greet() {
+//   console.log("Hello World");
+// }
+// greet();
+// let a = 1;
+// while (a < 5) {
+//   greet();
+//   a++;
+// }
+
+// const power = function (base, exponent) {
+// let result = 1;
+//   for (let count = 0; count < exponent; count++) {
+//     result = result * base;
+//   }
+//   return result;
+// };
+// console.log(power(2, 1));
+
+const hummus = function (factor) {
+  const ingredient = function (amount, unit, name) {
+    let ingredientAmount = amount * factor;
+    if (ingredientAmount > 1) {
+      unit += "s";
+    }
+    console.log(`${ingredientAmount} ${unit} ${name}`);
+  };
+  ingredient(1, "can", "chickpeas");
+  ingredient(0.25, "cup", "tahini");
+  ingredient(0.25, "cup", "lemon juice");
+  ingredient(1, "clove", "garlic");
+  ingredient(2, "tablespoon", "olive oil");
+  ingredient(0.5, "teaspoon", "cumin");
+};
+hummus(1);
