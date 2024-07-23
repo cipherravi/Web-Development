2e8661c7b07dd7cb88238a61e82e4edba2b145a6
cipherravi <ravi4personal9431@gmail.com>
2024-07-23 21:45:49 +0530

    added backend and added tailwind css

--- Practice/july_2024/Practice_1/Javascript/app.js
+++ Practice/july_2024/Practice_1/Javascript/app.js
@@ -1,1 +1,24 @@
-console.log("Hello Wolrd!");
+//// DOM
+
+// console.dir(document);
+
+////getElementsByTagName
+
+// console.log(document.getElementsByTagName("h1"));
+
+////getElementById
+
+// console.log(document.getElementById("main"));
+
+////getElementsByClassName
+
+// console.log(document.getElementsByClassName("group"));
+
+////querySelector
+
+// console.log(document.querySelector(".selectgroup"));
+// console.log(document.querySelector("#select"));
+
+////querySelectorAll
+
+// console.log(document.querySelectorAll("li"));
