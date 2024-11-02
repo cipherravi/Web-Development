94b737051c84158204797db7ca1d967b278abb4d
cipherravi <ravi4personal9431@gmail.com>
2024-11-02 12:36:44 +0530

    arranging all files

--- Projects/Portfolio/app.js
+++ Projects/Portfolio/app.js
@@ -1,2 +1,23 @@
-alert("hello");
-// console.log("first");
+let tl1=gsap.timeline();
+
+tl1.from(".menu-container li ",{
+    y:-20,
+    x:-8,
+    duration:0.6,
+    stagger:0.2,
+    opacity:0,
+    ease:"power1",
+
+})
+tl1.from (".nav .submit",{
+    x:200,
+    delay:-0.9,
+    duration:1,
+    opacity:0
+})
+tl1.from (".nav .logo",{
+    x:-200,
+    delay:-0.9,
+    duration:1,
+    opacity:0
+})
